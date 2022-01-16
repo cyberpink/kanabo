0808025076d3f26db464e0d2a4c34863801ff860
cyberpink <cozyflame42+github@gmail.com>
2022-01-16 04:49:15 -0600

    change sparse maps to use first class modules

--- src/sparse_map.ml
+++ src/sparse_map.ml
@@ -1,41 +1,53 @@
-module type SparseMapBase = sig
-  type 'a t
-  type key
-  val is_empty : 'a t -> bool
-  val empty : 'a t
-  val find_opt : key -> 'a t -> 'a option
-  val add : key -> 'a -> 'a t -> 'a t
-  val update : key -> ('a option -> 'a option) -> 'a t -> 'a t
-  val union : (key -> 'a -> 'a -> 'a option) -> 'a t -> 'a t -> 'a t
-  val intersection : (key -> 'a -> 'b -> 'c option) -> 'a t -> 'b t -> 'c t
-  val fold : (key -> 'a -> 'b -> 'b) -> 'a t -> 'b -> 'b
-  val filter_map : (key -> 'a -> 'b option) -> 'a t -> 'b t
-  val iter : (key -> 'a -> unit) -> 'a t -> unit
+module type HKMap = sig
+  type ('k, 'v) t
+
+  include module type of HK.Make2(struct type nonrec ('k, 'v) t = ('k, 'v) t end)
+
+  val add : 'k -> 'v -> ('k, 'v) t -> ('k, 'v) t
+  val find_opt : 'k -> ('k, 'v) t -> 'v option
+  val update : 'k -> ('v option -> 'v option) -> ('k, 'v) t -> ('k, 'v) t
 end
 
-module type SparseElem = sig
-  type t
-  val to_opt : t -> t option
-  val of_opt : t option -> t
+let find (type m v) (module M : HKMap with type hk = m)
+    (module E : Optional.S with type t = v) k a =
+  E.of_opt @@ M.find_opt k (M.prj a)
+
+let add (type m v) (module M : HKMap with type hk = m)
+    (module E : Optional.S with type t = v) k v a =
+  match E.to_opt v with None -> a | Some v -> M.inj (M.add k v (M.prj a))
+
+let update (type m v) (module M : HKMap with type hk = m)
+    (module E : Optional.S with type t = v) k f a =
+  M.inj @@ M.update k (fun x -> E.(to_opt (f (of_opt x)))) (M.prj a)
+
+module type Mappable = sig
+  include HKMap
+  val filter_map : ('k -> 'a -> 'b option) -> ('k, 'a) t -> ('k, 'b) t
 end
 
-module Make(M : SparseMapBase)(E : SparseElem) = struct
-  type t = E.t M.t      
-  type key = M.key
-  type elem = E.t
-           
-  let empty = M.empty
-  let is_empty = M.is_empty
-  let add k v a = match E.to_opt v with None -> a | Some v -> M.add k v a
-  let singleton k v = add k v empty
-  let find k a = E.of_opt @@ M.find_opt k a
-  let update k f a = M.update k (fun x -> E.(to_opt (f (of_opt x)))) a
-  let map f a = M.filter_map (fun _ e -> E.to_opt (f e)) a      
-  let union f a b = M.union (fun k av bv -> E.to_opt (f k av bv)) a b
-  let intersection f a b = M.intersection (fun k av bv -> E.to_opt (f k av bv)) a b
-  let map2 f a b = M.intersection (fun _ av bv -> E.to_opt (f av bv)) a b
-  let fold = M.fold
-  let iter = M.iter
-  let to_opt m = if M.is_empty m then None else Some m
-  let of_opt = function None -> empty | Some m -> m
+let map
+   (type m v)
+    (module M : Mappable with type hk = m)
+    (module E : Optional.S with type t = v) f a =
+  M.(inj @@ filter_map (fun _ e -> E.to_opt (f e)) (prj a))
+
+module type Mergeable = sig
+  include HKMap
+  val union : ('k -> 'a -> 'a -> 'a option) -> ('k, 'a) t -> ('k, 'a) t -> ('k, 'a) t
+  val intersection :
+    ('k -> 'a -> 'b -> 'c option) -> ('k, 'a) t -> ('k, 'b) t -> ('k, 'c) t
 end
+
+let union
+   (type m v)
+    (module M : Mergeable with type hk = m)
+    (module E : Optional.S with type t = v)  f a b =
+  M.(inj @@ union (fun k av bv -> E.to_opt (f k av bv)) (prj a) (prj b))
+
+let intersection
+   (type m v)
+    (module M : Mergeable with type hk = m)
+    (module E : Optional.S with type t = v) f a b =
+  M.(inj @@ intersection (fun k av bv -> E.to_opt (f k av bv)) (prj a) (prj b))
+
+let map2 = intersection
